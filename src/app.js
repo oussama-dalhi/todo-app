1e83bd6128e9a3174d2c826c901e2344418a7118
oussama-dalhi <oussamadalhi00@gmail.com>
2026-04-23 05:05:34 +0100

    Identify items with Date.now() as Id

--- src/app.js
+++ src/app.js
@@ -8,28 +8,28 @@ function addTodo() {
     const todoText = todoInput.value.trim();
     if(todoText.length > 0) {
         const todoObject = {
+            id: Date.now(),
             text: todoText,
             completed: false
         }
         allTodos.push(todoObject);
         updateTodoList();
         saveTodos();
-        todoInput.value = '';
+        todoInput.value = ''; 
     }
 }
 
 function updateTodoList() {
     todoListUL.innerHTML = '';
     allTodos.forEach((todo, todoIndex) => {
-        const todoItem = createTodoItem(todo, todoIndex);
+        const todoItem = createTodoItem(todo);
         todoListUL.append(todoItem);
     })
 }
 
-function createTodoItem(todo, todoIndex) {
-    const todoId = 'todo-'+todoIndex;
+function createTodoItem(todo) {
+    const todoId = 'todo-' + todo.id;
     const todoLI = document.createElement('li');
-    const todotext = todo.text;
     todoLI.className = 'todo';
     todoLI.innerHTML = `
         <input type="checkbox" id="${todoId}">
@@ -37,7 +37,7 @@ function createTodoItem(todo, todoIndex) {
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
         </label>
         <label for="${todoId}" class="todo-text">
-        ${todotext}
+        ${todo.text}
         </label>
         <button class="delete-button">
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
@@ -46,11 +46,11 @@ function createTodoItem(todo, todoIndex) {
     `;
     const deleteButton = todoLI.querySelector('.delete-button');
     deleteButton.addEventListener('click', () => {
-        deleteTodoItem(todoIndex);
+        deleteTodoItem(todo.id);
     })
     const checkbox = todoLI.querySelector('input');
     checkbox.addEventListener('change', () => {
-        allTodos[todoIndex].completed = checkbox.checked;
+        todo.completed = checkbox.checked;
         updateTaskCounter()
         saveTodos();
     })
@@ -58,8 +58,8 @@ function createTodoItem(todo, todoIndex) {
     return todoLI;
 }
 
-function deleteTodoItem(todoIndex) {
-    allTodos = allTodos.filter((_, i) => i !== todoIndex);
+function deleteTodoItem(id) {
+    allTodos = allTodos.filter(todo => todo.id !== id);
     saveTodos();
     updateTodoList();
     updateTaskCounter();
